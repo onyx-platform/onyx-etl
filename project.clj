a002b7db88a378d6b65f71e968d3ec8c0ac6278e
Michael Drogalis <mjd3089@rit.edu>
2015-12-06 18:23:47 -0800

    Upgrade etl-support.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-etl/lein-template "0.8.2.1"
+(defproject onyx-etl/lein-template "0.8.2.2"
   :description "Extract, transform, load convenience Onyx package"
   :url "https://github.com/onyx-platform/onyx-etl"
   :license {:name "Eclipse Public License"
